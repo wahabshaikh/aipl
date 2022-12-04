69a543806b998c05f9857b37da4f1cf2137b4140
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2022-12-05 02:29:22 +0530

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Toaster />
+      <Analytics />
     </>
   );
 }
