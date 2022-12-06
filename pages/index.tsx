4b4861948c58f7d45a6ad76f1c2b3680d859fce3
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2022-12-06 16:35:06 +0530

    feat: add product hunt badges

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,6 @@ const Home: NextPage = () => {
 
   const generatePickupLine = async (keyword: string) => {
     setIsLoading(true);
-    setCount(count + 1);
 
     try {
       const data = await toast.promise(
@@ -79,6 +78,7 @@ const Home: NextPage = () => {
       console.error(error);
     } finally {
       setIsLoading(false);
+      setCount(count + 1);
     }
   };
 
@@ -223,6 +223,28 @@ const Home: NextPage = () => {
               Surprise me!
             </button>
           </fieldset>
+          <div className="mt-8 grid gap-4 sm:grid-cols-2">
+            <a
+              href="https://www.producthunt.com/posts/ai-pickup-lines-generator?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-ai&#0045;pickup&#0045;lines&#0045;generator"
+              target="_blank"
+            >
+              <img
+                src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=369828&theme=light&period=daily"
+                alt="AI&#0032;Pickup&#0032;Lines&#0032;Generator - Smoothest&#0032;pickup&#0032;lines&#0032;ever&#0032;&#0045;&#0032;powered&#0032;by&#0032;AI | Product Hunt"
+                className="h-full w-full"
+              />
+            </a>
+            <a
+              href="https://www.producthunt.com/posts/ai-pickup-lines-generator?utm_source=badge-top-post-topic-badge&utm_medium=badge&utm_souce=badge-ai&#0045;pickup&#0045;lines&#0045;generator"
+              target="_blank"
+            >
+              <img
+                src="https://api.producthunt.com/widgets/embed-image/v1/top-post-topic-badge.svg?post_id=369828&theme=light&period=weekly&topic=Artificial Intelligence"
+                alt="AI&#0032;Pickup&#0032;Lines&#0032;Generator - Smoothest&#0032;pickup&#0032;lines&#0032;ever&#0032;&#0045;&#0032;powered&#0032;by&#0032;AI | Product Hunt"
+                className="h-full w-full"
+              />
+            </a>
+          </div>
         </section>
       </main>
       <Modal count={count} />
