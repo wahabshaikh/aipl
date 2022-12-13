0f5f6ded9e4408427e32f60163f2e10b071508d0
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2022-12-13 12:20:32 +0530

    fix: move analytics from _document.tsx to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,25 @@ import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
 import ReactTooltip from "react-tooltip";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-6SGJEJRLQJ"
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6SGJEJRLQJ', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Component {...pageProps} />
       <ReactTooltip effect="solid" />
       <Toaster />
