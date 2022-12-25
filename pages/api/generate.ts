6cdfdca82c5d5f8fb3cfcdbbbe9fbbc96fe632be
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2022-12-26 01:01:24 +0530

    fix: optimise the generate api

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
+import { supabase } from "../../lib/supabase";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -15,22 +16,36 @@ export default async function handler(
   try {
     const keyword = req.body.keyword as string;
 
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `Write a punny pickup line on ${keyword}.`,
-      temperature: 0.7,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      best_of: 1,
-      max_tokens: 256,
-    });
+    const { data, error } = await supabase
+      .from("pickup_lines")
+      .select("result")
+      .eq("keyword", keyword);
 
-    const result = completion.data.choices[0].text?.trim();
+    if (error) throw error;
 
-    if (!result) throw new Error(`Rate limit exceeded`);
+    if (data && data.length > 0) {
+      const filteredData = data.filter(({ result }) => result != "");
+      const result =
+        filteredData[Math.floor(Math.random() * filteredData.length)].result;
 
-    res.status(200).json({ result });
+      res.status(200).json({ result });
+    } else {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Write a punny pickup line on ${keyword}.`,
+        temperature: 0.7,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        best_of: 1,
+        max_tokens: 256,
+      });
+
+      const result = completion.data.choices[0].text?.trim();
+      if (!result) throw new Error(`No result found!`);
+
+      res.status(200).json({ result });
+    }
   } catch (error) {
     console.error(error);
     res.status(408).json({ result: "" });
